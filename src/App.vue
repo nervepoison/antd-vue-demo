710de2418c62c862fb10712982b34726981f3e2a
kaido <caoyt688@gmail.com>
2020-01-16 17:10:13 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <a-locale-provider :locale="locale">
     <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
       <router-view />
-      <a-pagination :defaultCurrent="1" :total="50" showSizeChanger />
-      <a-button v-on:click="click()" type="primary">button</a-button>
     </div>
   </a-locale-provider>
 </template>
@@ -21,12 +15,7 @@ export default {
     };
   },
   mounted() {},
-  methods: {
-    click() {
-      alert();
-      console.log(this.locale);
-    }
-  }
+  methods: {}
 };
 </script>
 
@@ -35,7 +24,7 @@ export default {
   height: 100%;
 }
 
-#nprogress .bar {
-  //background: #118060 !important; //自定义颜色
-}
+// #nprogress .bar {
+//background: #118060 !important; //自定义颜色
+// }
 </style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-layout :class="['layout', device]">
+    <!-- menu -->
+    <a-drawer
+      v-if="isMoble()"
+      placement="left"
+      :wrapClassName="`drawer-sider ${navTheme}`"
+      :closable="false"
+      :visible="collapsed"
+      @close="drawerClose"
+    >
+      <side-menu
+        mode="inline"
+        :menus="menus"
+        :theme="navTheme"
+        :collapsed="false"
+        :collapsible="true"
+        @menuSelect="menuSelect"
+      >
+      </side-menu>
+    </a-drawer>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  name: "BaseLayout",
+  date() {
+    return {};
+  }
+};
+</script>
